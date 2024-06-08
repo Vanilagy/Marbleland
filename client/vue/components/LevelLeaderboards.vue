f90817530bdb692c7b749dd6ebb9015acfa44ad7
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2024-06-08 22:33:35 +0530

    remove pagination

--- client/vue/components/LevelLeaderboards.vue
+++ client/vue/components/LevelLeaderboards.vue
@@ -1,36 +1,33 @@
 <template>
-    <table>
-    	<thead>
-    		<tr>
-    			<th>#</th>
-    			<th>Name</th>
-    			<th>Score</th>
-    		</tr>
-    	</thead>
-    	<tbody v-if="leaderboardsView.length !== 0">
-			<tr v-for="record of leaderboardsView">
-				<td>{{ record.rank }}</td>
-				<td>{{ record.name }}</td>
-				<td>{{ record.score }}</td>
-			</tr>
-    	</tbody>
-		<tbody v-if="status !== ''">
-			<tr>
-				<td colspan="3">{{ status }}</td>
-			</tr>
-		</tbody>
-    </table>
-    <div class="buttonContainer" v-if="leaderboardsView.length !== 0">
-        <button-with-icon :title="previous" ref="button" class="button" :icon="previousIcon" noMargin @click="page = Math.max(0, page - 1)" :style="{visibility: page === 0 ? 'hidden' : ''}"></button-with-icon>
-        <span style="margin: auto;">Page {{ page + 1 }}/{{ Math.ceil(scores.length / scoresPerPage) }}</span>
-        <button-with-icon :title="next" ref="button" class="button" :icon="nextIcon" noMargin @click="page = Math.min(Math.ceil(scores.length / scoresPerPage) - 1, page + 1)" :style="{visibility: page === Math.ceil(scores.length / scoresPerPage) - 1 ? 'hidden' : ''}"></button-with-icon>
-    </div>
+	<div class="container">
+		<table>
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>Name</th>
+					<th>Score</th>
+				</tr>
+			</thead>
+			<tbody v-if="leaderboardsView.length !== 0">
+				<tr v-for="record of leaderboardsView">
+					<td>{{ record.rank }}</td>
+					<td>{{ record.name }}</td>
+					<td>{{ record.score }}</td>
+				</tr>
+			</tbody>
+			<tbody v-if="status !== ''">
+				<tr>
+					<td colspan="3">{{ status }}</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import ButtonWithIcon from './ButtonWithIcon.vue';
-import { LeaderboardDefinition, LeaderboardScore } from '../../../shared/types';
+import { LeaderboardScore } from '../../../shared/types';
 import { Util } from '../../ts/util';
 
 export default defineComponent({
@@ -47,13 +44,11 @@ export default defineComponent({
             previous: "Previous",
             nextIcon: "/assets/svg/navigate_next_black_24dp.svg",
 			previousIcon: "/assets/svg/navigate_before_black_24dp.svg",
-			page: 0,
-			scoresPerPage: 10
         };
 	},
 	computed: {
 		leaderboardsView(): { rank: number, name: string, score: string }[] {
-			return this.scores.slice(this.page * this.scoresPerPage, (this.page + 1) * this.scoresPerPage).map((record, i) => {
+			return this.scores.map((record, i) => {
 				return {
 					rank: record.placement,
 					name: record.username,
@@ -66,6 +61,10 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.container {
+	height: 400px;
+	overflow-y: auto;
+}
 table {
     width: 100%;
     background: var(--background-1);
@@ -74,6 +73,8 @@ table {
 }
 thead {
     background: var(--background-2);
+	position: sticky;
+	top: 0px;
 }
 thead tr th:first-child {
 	width: 10%;
@@ -89,14 +90,5 @@ tbody tr td:first-child {
 }
 tbody tr td:last-child {
 	text-align: center;
-}
-.buttonContainer {
-	display: flex;
-	width: 100%;
-	margin-top: 10px;
-    justify-content: space-between;
-}
-.buttonContainer > div {
-    width: 40px;
 }
 </style>
